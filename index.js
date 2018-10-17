6b9ea8ae72ba78098d5b7293508a644b8f165064
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2018-10-17 21:00:43 +0530

    updated comments and cleaned the code

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-var   express = require('express')
-	, http = require('http')
-	, async = require('async')
-	, multer = require('multer')
-	, upload = multer({dest: 'uploads/'})
-	, exphbs = require('express-handlebars')
-	, easyimg = require('easyimage')
-	, _ = require('lodash')
-	, cv = require('opencv');
+//  Importing the Libraries
+const express = require('express'),
+      http    = require('http'), 
+      async   = require('async'), 
+      multer  = require('multer'), 
+      upload  = multer({dest: 'uploads/'}), 
+      exphbs  = require('express-handlebars'), 
+      easyimg = require('easyimage'), 
+      _       = require('lodash'), 
+      cv      = require('opencv');
 
 
 // Image upload exts
-
 var exts = {
-	'image/jpeg': '.jpg',
+  'image/jpeg': '.jpg',
 	'image/png' : '.png',
 	'image/gif' : '.gif'
 };
 
 
-var port = 8090;
+const port = 8090;
 var app = express();
 app.use(express.static(__dirname + '/public'))
 
-/**
- * Set up Handlebars templating
- */
+//  Set up Handlebars templating
 app.engine('.hbs', exphbs( { extname: '.hbs', defaultLayout: 'default' } ) );
 app.set( 'view engine', '.hbs' );
 
 
 //**IMPORTANTE PARTE** POST callback 
-
 app.post('/upload', upload.single('file'), function(req, res, next){
 
 	//copy filename + ext
@@ -40,10 +37,9 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 	, dst = __dirname + '/public/images/' + filename;
 
 
-    
   async.waterfall(
     [
-    //Is it an image?? (src)
+    //  Is it an image?? (src)
       function( callback ) {
 
         
@@ -64,7 +60,7 @@ app.post('/upload', upload.single('file'), function(req, res, next){
 
       },
 
-    //Is it large enough? (src)
+    //  Is it large enough? (src)
       function( callback ) {
 
           easyimg.info( src ).then(
@@ -82,7 +78,7 @@ app.post('/upload', upload.single('file'), function(req, res, next){
         );
       },
 
-    //Resize image to 960 px (save to dst)'
+    //  Resize image to 960 px (save to dst)'
       function( callback ) {
       easyimg.resize(
           {
@@ -93,10 +89,10 @@ app.post('/upload', upload.single('file'), function(req, res, next){
         ).then(function(image) { return callback(); });
       },
 
-    /** OPENCV STUFF: 'faces' is an array of hashes (x and y coordinates, width and height,) about found faces:
-        
+    /** OPENCV STUFF: 'faces' is an array of hashes (x and y coordinates, width and height,) 
+        about found faces:
         im.detectObject(classifier, options, function(err, faces) {  }); **/
-    
+
     //Retrieve file from dst
       function(callback){
         cv.readImage(dst, callback);
@@ -120,22 +116,22 @@ app.post('/upload', upload.single('file'), function(req, res, next){
         return res.render (
           'result', {filename : filename, faces :faces}
         );
-      
       }
-
     ); // waterfall
-
 }); // app.post
 
 
-
-
-http.createServer(app)
-	.listen(port, function(server) {
-		console.log('Listening on port %d', port);
-	});
-
+//  default route
 app.get('/', function( req, res, next) {
-	return res.render('index');
+  return res.render('index');
+});
+
+app.get('*',function(req, res){
+  res.redirect('/');
+})
+
+
+http.createServer(app).listen(port, function(server) {
+		console.log('Listening on port %d', port);
 });
 
